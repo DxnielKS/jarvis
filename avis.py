ae245928e5db9f3a5c1b13c03ec3324cbdb3ca03
DanielKS <47062269+DxnielKS@users.noreply.github.com>
2019-09-27 12:43:41 +0100

    Add files via upload

--- avis.py
+++ avis.py
@@ -1,36 +1,33 @@
-import pygame
+###################IMPORTS##########################
+
+import pygame, sys, smtplib, webbrowser, os, time, speech_recognition as sr
 from pygame.locals import *
-import sys
-import smtplib
-import webbrowser
-import os
-import time
 from gtts import gTTS
-import speech_recognition as sr
 
-###################################################
+###################DISPLAY##########################
 
-pygame.init()
+pygame.init() # initialising the pygame module
+clock = pygame.time.Clock()
 FPS = 60 # The frames per second
 WINDOWX,WINDOWY= 300,150 # These are the dimensions for the Avis window
 WINDOWBORDERWIDTHX,WINDOWBORDERWIDTHY= 7,23 # Dimensions for the border of the window border
-
-###################################################
-
-BLACK = (0,0,0)
-WHITE = (255,255,255)
 RESOLUTION =(2560,1600) # Resolution of the screen
 """w,h=pygame.display.get_window_size()"""
-x = RESOLUTION[0]-WINDOWX-WINDOWBORDERWIDTHX
+x = RESOLUTION[0]-WINDOWX-WINDOWBORDERWIDTHX # The width of the entire GUI
 y = WINDOWBORDERWIDTHY
-
-###################################################
-
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
 DISPLAY = pygame.display.set_mode((WINDOWX,WINDOWY))
 pygame.display.set_caption("Avis")
 
-###################################################
+####################COLOURS#########################
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+####################MAIN############################
 
 def talkToMe(audio):
   print (audio)
@@ -38,9 +35,8 @@ def talkToMe(audio):
   tts.save("audio.mp3")
   mixer.init()
   mixer.music.load("audio.mp3")
-  mixer.music.play()
-# listens for command
-
+  mixer.music.play() # plays the audio file from
+# text to speech module
 def myCommand():
   r = sr.Recognizer()
   with sr.Microphone() as source:
@@ -56,7 +52,7 @@ def myCommand():
     print ("[!] I didn't quite get that!")
     assistant(myCommand())
   return command
-
+# takes the audio from the user as a command
 def background():
   r = sr.Recognizer()
   with sr.Microphone() as source:
@@ -70,11 +66,18 @@ def background():
     background()
   if "what's up" in text:
     myCommand()
-
+# listens to user in the background
+def decipher():
+    None
+# algorithm to decide what function to carry out
 while True:
-        pygame.draw.circle(DISPLAY,WHITE,(150,135),10,3)
-        for event in pygame.event.get():
-                if event.type == QUIT:  
-                        pygame.quit()
-                        sys.exit()
-        pygame.display.flip()
+    DISPLAY.fill(WHITE)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit() # close window
+    pygame.draw.circle(DISPLAY,WHITE,(150,135),10,3) # button circle
+    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(FPS)
+# main program loop
