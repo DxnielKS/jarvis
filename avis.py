85ae5113499818c1158d709bbf2c419db79b9190
DanielKS <47062269+DxnielKS@users.noreply.github.com>
2019-10-13 08:36:41 +0100

    Add files via upload

--- avis.py
+++ avis.py
@@ -1,6 +1,12 @@
 ###################IMPORTS##########################
 
-import pygame, sys, smtplib, webbrowser, os, time, speech_recognition as sr
+import pygame
+import sys
+import smtplib
+import webbrowser
+import os
+import time
+import speech_recognition as sr
 from pygame.locals import *
 from gtts import gTTS
 
@@ -10,7 +16,7 @@ pygame.init() # initialising the pygame module
 clock = pygame.time.Clock()
 FPS = 60 # The frames per second
 WINDOWX,WINDOWY= 300,150 # These are the dimensions for the Avis window
-WINDOWBORDERWIDTHX,WINDOWBORDERWIDTHY= 7,23 # Dimensions for the border of the window border
+WINDOWBORDERWIDTHX,WINDOWBORDERWIDTHY= 7,0 # Dimensions for the border of the window border
 RESOLUTION =(2560,1600) # Resolution of the screen
 """w,h=pygame.display.get_window_size()"""
 x = RESOLUTION[0]-WINDOWX-WINDOWBORDERWIDTHX # The width of the entire GUI
@@ -18,66 +24,48 @@ y = WINDOWBORDERWIDTHY
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
 DISPLAY = pygame.display.set_mode((WINDOWX,WINDOWY))
 pygame.display.set_caption("Avis")
+icon = pygame.image.load("logo.png")
+pygame.display.set_icon(icon)
 
 ####################COLOURS#########################
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+GREY = (192,192,192)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 
 ####################MAIN############################
 
-def talkToMe(audio):
-  print (audio)
-  tts = gTTS(text=audio, lang = "en")
-  tts.save("audio.mp3")
-  mixer.init()
-  mixer.music.load("audio.mp3")
-  mixer.music.play() # plays the audio file from
-# text to speech module
-def myCommand():
+def textToSpeech(text): # converts text into speech
+  print (text)
+  tts = gTTS(text=text, lang = "en")
+  tts.save("speech.mp3")
+  pygame.mixer.init()
+  pygame.mixer.music.load("speech.mp3")
+  pygame.mixer.music.play()
+def speechToText(): # converts speech to text
   r = sr.Recognizer()
   with sr.Microphone() as source:
-    talkToMe("I am ready for your next command!")
-    r.pause_thresh = 1
-    r.adjust_for_ambient_noise(source, duration = 1)
-    audio = r.listen(source)
-  try:
-    command = r.recognize_google(audio)
-    print ("You said: %s" % command)
-  # loop back to continue to listen for commands
-  except sr.UnknownValueError:
-    print ("[!] I didn't quite get that!")
-    assistant(myCommand())
-  return command
-# takes the audio from the user as a command
-def background():
-  r = sr.Recognizer()
-  with sr.Microphone() as source:
-    talkToMe("I am ready for your next command!")
     r.pause_thresh = 1
     r.adjust_for_ambient_noise(source, duration = 1)
     audio = r.listen(source)
   try:
     text = r.recognize_google(audio)
-  except:
-    background()
-  if "what's up" in text:
-    myCommand()
-# listens to user in the background
-def decipher():
-    None
-# algorithm to decide what function to carry out
-while True:
+    print ("You said: %s" % text)
+  # loop back to continue to listen for speech
+  except sr.UnknownValueError:
+    print ("[!] I didn't quite get that!")
+    speechToText()
+  return text
+while True: # main program loop
     DISPLAY.fill(WHITE)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit() # close window
-    pygame.draw.circle(DISPLAY,WHITE,(150,135),10,3) # button circle
+    pygame.draw.circle(DISPLAY,GREY,(150,135),10,3) # button circle
     pygame.display.flip()
     pygame.display.update()
     clock.tick(FPS)
-# main program loop
